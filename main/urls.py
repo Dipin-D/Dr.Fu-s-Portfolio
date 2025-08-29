7f7bcbf000d63ffa5671af3c78e4b550af424758
Dipin-D <dipindawadi@gmail.com>
2025-08-29 16:51:16 -0500

    research project page added

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  
+    path('researchprojects/', views.researchprojects, name='researchprojects'),
+    path('researchprojects/project1/', views.project1, name='project1'),
+    path('researchprojects/project2/', views.project2, name='project2'),
+    path('researchprojects/project3/', views.project3, name='project3'),
+    path('researchprojects/project4/', views.project4, name='project4'),
+    path('researchprojects/project5/', views.project5, name='project5'),  
     path('publications/', views.publications, name='publications'),
     path('courses/', views.courses, name='courses'),
     path('software/', views.software, name='software'),
--- main/views.py
+++ main/views.py
@@ -5,6 +5,25 @@ def base(request):
     return render(request, 'base.html')
 def home(request):
     return render(request, 'home.html')
+
+def researchprojects(request):
+    return render(request, 'researchprojects.html')
+
+def project1(request):
+    return render(request, 'researchprojects/project1.html')
+
+def project2(request):
+    return render(request, 'researchprojects/project2.html')
+
+def project3(request):
+    return render(request, 'researchprojects/project3.html')
+
+def project4(request):
+    return render(request, 'researchprojects/project4.html')
+
+def project5(request):
+    return render(request, 'researchprojects/project5.html')
+
 def publications(request):
     return render(request, 'publications.html')
 
